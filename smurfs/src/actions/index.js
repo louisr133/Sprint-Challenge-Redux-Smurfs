36c6e425939ec51b16e30872f8b824c2f6cae3c0
louisroman <louisroman133@gmail.com>
2019-04-19 12:26:44 -0400

    mvp

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,15 +1,51 @@
-/* 
-  Action Types Go Here!
-  Be sure to export each action type so you can pull it into your reducer
-*/
+import axios from 'axios'
 
-/*
-  For this project you'll need at least 2 action creators for the main portion,
-   and 2 more for the stretch problem.
-   Be sure to include action types for each type of action creator. Also, be sure to mind
-     the "pending" states like, fetching, creating, updating and deleting.
-   C - addSmurf
-   R - getSmurfs
-   U - updateSmurf
-   D - deleteSmurf
-*/
+export const FETCH_START = 'FETCH_START'
+export const FETCH_SUCCESS = 'FETCH_SUCCESS'
+export const FETCH_FAIL = 'FETCH_FAIL'
+
+export const POST_START = 'POST_START'
+export const POST_SUCCESS = 'POST_SUCCESS'
+export const POST_FAIL = 'POST_FAIL'
+
+export const fetchSmurfs = () => dispatch => {
+  dispatch({
+    type: FETCH_START,
+  })
+
+  axios
+  .get('http://localhost:3333/smurfs')
+  .then(res => {
+    dispatch({
+      type: FETCH_SUCCESS,
+      payload :res.data
+    })
+  })
+    .catch(err => {
+      dispatch({
+        type: FETCH_FAIL,
+        payload :err.data
+      })
+    })
+}
+
+export const postSmurfs = (smurf) => dispatch => {
+  dispatch({
+    type: POST_START,
+  })
+
+  axios
+  .post('http://localhost:3333/smurfs', smurf)
+  .then(res => {
+    dispatch({
+      type: POST_SUCCESS,
+      payload :res.data
+    })
+  })
+    .catch(err => {
+      dispatch({
+        type: POST_FAIL,
+        payload :err.data
+      })
+    })
+}
--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,22 +1,104 @@
 import React, { Component } from 'react';
 import './App.css';
-/*
- to wire this component up you're going to need a few things.
- I'll let you do this part on your own. 
- Just remember, `how do I `connect` my components to redux?`
- `How do I ensure that my component links the state to props?`
- */
+import {connect} from 'react-redux';
+import {fetchSmurfs, postSmurfs} from '../actions'
 class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      name: '',
+      age: '',
+      height: '',
+      // id: '',
+    
+    }
+
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      ...this.state,
+      [e.target.name]: e.target.value,
+    })
+  }
+
+  postSmurfButton = (e,smurf) => {
+    e.preventDefault();
+    this.props.postSmurfs(smurf);
+
+  }
+  componentDidMount(){
+    this.props.fetchSmurfs()
+  }
+
   render() {
     return (
       <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your Redux version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
+        {
+          this.props.smurfs.map(smurf => {
+            return (
+              <div className='smurfBox'>
+                <h1>Name: {smurf.name}</h1>
+                <h1>Age: {smurf.age}</h1>
+                <h1>Height: {smurf.height}</h1>
+                {/* <h1>{smurf.id}</h1> */}
+              </div>
+            )
+          })
+        }
+
+        <form onSubmit = {(e) => this.postSmurfButton(e,this.state)}>
+          <input
+            type='text'
+            name='name'
+            placeholder='name'
+            value={this.state.name}
+            onChange={(e) => this.handleChange(e)}
+          
+          />
+
+          <input
+            type='text'
+            name='age'
+            placeholder='age'
+            value={this.state.age}
+            onChange={(e) => this.handleChange(e)}
+          />
+          <input
+            type='text'
+            name='height'
+            placeholder='height'
+            value={this.state.height}
+            onChange={(e) => this.handleChange(e)}
+          
+          />
+
+          {/* <input
+            type='text'
+            name='id'
+            placeholder='id'
+            value={this.state.id}
+            onChange={(e) => this.handleChange(e)}
+          
+          /> */}
+
+          <button >POST NEW SMURF</button>
+
+
+        </form>
+
+
+
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return { 
+    ...state,
+    smurfs: [...state.smurfs],
+  }
+}
+export default connect(mapStateToProps, {fetchSmurfs, postSmurfs})(App);
